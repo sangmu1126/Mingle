2f889c10f8b7e89524d06d2537a3cd6a14aa448b
sangmu1126@gmail.com <sangmu1126@gmail.com>
2024-04-09 23:07:17 +0900

    Mingle BackEnd

---
+++ spring/mingle/src/main/java/gdsc/mingle/MingleApplication.java
@@ -0,0 +1,13 @@
+package gdsc.mingle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MingleApplication.class, args);
+	}
+
+}
---
+++ spring/mingle/src/test/java/gdsc/mingle/MingleApplicationTests.java
@@ -0,0 +1,13 @@
+package gdsc.mingle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MingleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
